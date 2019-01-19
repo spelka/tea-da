ad1f3968cd92d2c6843ec5d03c7de026780d0a86
spelka <sebastian.pelka@hotmail.com>
2019-01-19 11:27:03 -0800

    Initial Commit

---
+++ app/src/main/java/ca/sebon/tea_da/MainActivity.java
@@ -0,0 +1,13 @@
+package ca.sebon.tea_da;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
