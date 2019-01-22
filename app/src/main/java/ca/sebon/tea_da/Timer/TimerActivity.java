88bc04b8157a4add0505bd14fec1796a60ac816a
spelka <sebastian.pelka@hotmail.com>
2019-01-22 11:30:56 -0800

    Initial commit for the timer activity class and manifest update

--- app/src/main/java/ca/sebon/tea_da/Timer/TimerActivity.java
+++ app/src/main/java/ca/sebon/tea_da/Timer/TimerActivity.java
@@ -1,4 +1,27 @@
 package ca.sebon.tea_da.Timer;
 
-public class TimerActivity {
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.Chronometer;
+
+import ca.sebon.tea_da.Database.Tea;
+import ca.sebon.tea_da.R;
+
+public class TimerActivity extends AppCompatActivity
+{
+    private static final String TAG = "TimerActivity";
+    
+    private Tea currentTea;
+    private Chronometer chronometer;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        Log.d(TAG, "onCreate: started");
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_timer);
+        Log.d(TAG, "onCreate: finished");
+    }
 }
