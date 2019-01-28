72e99b0b84c320aba5dda467d743386961532e1c
spelka <sebastian.pelka@hotmail.com>
2019-01-28 12:24:34 -0800

    Fixed out of memory bug by resizing title image with picasso library

--- app/src/main/java/ca/sebon/tea_da/Main/MainActivity.java
+++ app/src/main/java/ca/sebon/tea_da/Main/MainActivity.java
@@ -5,6 +5,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
+
+import java.io.File;
 
 import ca.sebon.tea_da.Menu.MenuActivity;
 import ca.sebon.tea_da.R;
@@ -17,6 +22,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView imageView = findViewById(R.id.main_image_view);
+        Picasso.with(this)
+                .load(R.drawable.tea_da_logo)
+                .fit()
+                .into(imageView);
     }
 
     public void loadMenuActivity(View view)
